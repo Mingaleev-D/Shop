c92c1a2318af34395b968405125ad7b5e4715fb9
mingaleev.d92@mail.ru <tatarstan1204BTc)>
2022-10-10 03:27:26 +0300

    refactoring functions

--- src/main/kotlin/studev/Main.kt
+++ src/main/kotlin/studev/Main.kt
@@ -1,61 +1,90 @@
 package studev
 
+val itemProductsNames = mutableListOf<String>()
+val itemAmounts = mutableListOf<String>()
+
+fun askUntilYesOrNo(message: String): String {
+   println(message)
+   var answer = readLine()!!
+
+   while (answer != "yes" || answer != "no") {
+      println(message)
+      println("please only enter yes or no")
+      answer = readLine()!!
+   }
+   return answer
+}
+
+fun createProducts() {
+   val productNames = mutableListOf<String>()
+   val productPrices = mutableListOf<String>()
+
+   do {
+      println("Enter product name: ")
+      val productName = readLine()!!
+      productNames.add(productName)
+
+      println("Enter product price: ")
+      val productPrice = readLine()!!
+      productPrices.add(productPrice)
+
+      val answer = askUntilYesOrNo("do you want do define another product? yes/no")
+
+   } while (answer != "no")
+}
+
+fun askTheItemsOfShoppingCart() {
+   do {
+      println("Enter item product: ")
+      val itemProductName = readLine()!!
+      itemProductsNames.add(itemProductName)
+
+      println("Enter amount of item: ")
+      val itemAmount = readLine()!!
+      itemAmounts.add(itemAmount)
+
+      val answer = askUntilYesOrNo(" Is there any item? yes/no")
+
+   } while (answer != "no")
+}
+
+fun printTheFinalReceipt() {
+   var indexOfItemProductNames = 0
+
+   do {
+      val thisItemProductName = itemProductsNames[indexOfItemProductNames]
+      val thisItemProductAmount = itemAmounts[indexOfItemProductNames]
+
+      indexOfItemProductNames++
+
+      println("item $itemProductsNames is $thisItemProductAmount of $thisItemProductName")
+
+   } while (indexOfItemProductNames < itemProductsNames.size)
+}
+
+fun askCustomerName() {
+   println("Enter customer name: ")
+   val customerName = readLine()!!
+}
+
+fun askShopName() {
+   println("Enter shop name: ")
+   val shopName = readLine()
+}
+
 fun main() {
 
-//    println("Enter shop name: ")
-//    val shopName = readLine()
-//    println("Shop name is: $shopName")
+   askShopName()
 
+   createProducts()
 
-    val productNames = mutableListOf<String>()
-    val productPrices = mutableListOf<String>()
+   askCustomerName()
 
-    do{
-        println("Enter product name: ")
-        val productName = readLine()!!
-        productNames.add(productName)
+   askTheItemsOfShoppingCart()
 
-        println("Enter product price: ")
-        val productPrice = readLine()!!
-        productPrices.add(productPrice)
-
-        println(" do you want do define another product? yes/no")
-        val answer = readLine()!!
-
-    } while (answer != "no")
-
-    println("Enter customer name: ")
-    val customerName = readLine()!!
-
-    val itemProductsNames = mutableListOf<String>()
-    val itemAmounts = mutableListOf<String>()
-
-    do{
-        println("Enter item product: ")
-        val itemProductName = readLine()!!
-        itemProductsNames.add(itemProductName)
-
-        println("Enter amount of item: ")
-        val itemAmount = readLine()!!
-        itemAmounts.add(itemAmount)
-
-
-        println(" Is there any item? yes/no")
-        val answer = readLine()!!
-
-    } while (answer != "no")
-
-    var indexOfItemProductNames = 0
-
-    do {
-        val thisItemProductName = itemProductsNames[indexOfItemProductNames]
-        val thisItemProductAmount = itemAmounts[indexOfItemProductNames]
-
-        indexOfItemProductNames++
-
-        println("item $itemProductsNames is $thisItemProductAmount of $thisItemProductName")
-
-    }while (indexOfItemProductNames < itemProductsNames.size)
-
+   printTheFinalReceipt()
 
 }
+
+
+
